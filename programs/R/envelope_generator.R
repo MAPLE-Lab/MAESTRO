80ce71801cbdcfef167115dd8a8e8578037be343
maxwellng <ngmc2@mcmaster.ca>
2017-06-21 23:43:20 -0400

    June 21 2017
    envelope_generator.R:
    - Re-organized to use functions from the source file rather than have all functions directly in the code file.
    - Most user inputs are at the beginning of the code, to make it clear where manual inputs are required vs where the code is automated for the analysis.
    - Currently only top-down analysis (specify fundamental frequency, number of harmonics, etc.). This can be made more rigorous. A down-up analysis (creates necessary harmonics based on what frequencies are being played) are future steps.
    
    functions_R.R:
    - Functionalized code.
    - Exports a single tab delimited file now, rather than many more specialized text files and tables.
    
    envelope_reader.scd:
    - New loop for reading the single tab delimited data exported from the R code.
    - Rather than use one SynthDef for 16 harmonics (which is too big for SC to read and even truncated caused audio rerrors), 16 SynthDefs are created and are sent in a single bundle (thus synchronized and able to read, without errors).
    - In the future there should be a way to turn this bundle into a loop as well, thereby allowing greater flexibility with the amount of harmonics sought.
    
    R_working_files:
    - The contents of this folder can all be deleted (but not the folder itself as it is needed by R to create files and by SC to read files). However, demo files for 5 instruments (cello, horn, flute, trumpet, clarinet) have been saved. To use these rather than the earliest user export from R, in envelope_reader.scd change the ~harmonicsDataArray defining line's path to "harmonicMatrixScaled-cello.txt" (for the cello, for example) rather than "harmonicMatrixScaled.txt".

--- programs/R/envelope_generator.R
+++ programs/R/envelope_generator.R
@@ -4,6 +4,15 @@
 # This line loads all the functions from the "functions_R.R" R code file.
 # These functions are then called (with appropriate arguments) in all following code.
 filePath <- "~/Dropbox/MAPLE-Lab-Auditory-Exploration-Suite/programs/R"
+frequencyResolution <- 4 # 4 is a good value with a frequency resolution of ~20Hz. 5 increases frequency resolution to ~10Hz.
+thresholdValue <- 0 # Set at 0 all noise is captured; however, in top-down analysis (where the user selects the harmonics), this is not a problem and improves audio synthesis
+# STEP 3 - SELECT SOUND
+# Select the sound file to be analyzed
+# Usage: Opens pop-up window for the user to select the sound file they want to analyze
+# Input: WAVE/WAV file (i.e. .wav)
+# Output: path to the sound file, saved as "soundfile" in R
+soundfile <- file.choose()
+
 setwd(filePath)
 source("functions_R.R")
 
@@ -17,12 +26,6 @@ loadPackages()
 # "R_working_files" is a directory pre-made in MAESTRO.
 organizeDirectory(filePath)
 
-# STEP 3 - SELECT SOUND
-# Select the sound file to be analyzed
-# Usage: Opens pop-up window for the user to select the sound file they want to analyze
-# Input: WAVE/WAV file (i.e. .wav)
-# Output: path to the sound file, saved as "soundfile" in R
-soundfile <- file.choose()
 
 # STEP 4 - STEREO CHECK
 # Check if the sound file is stereo
@@ -40,115 +43,50 @@ soundfileStereoCheck(soundfile)
 
 # Input: In the line "numberOfFreqBins <- 256*4", change the multiplier (i.e. 4) to the desired amount.
 # Output: wlValue will be used in following functions. freqBinWidth gives the frequency range of each bin.
-numberOfFreqBins <- 256*4
-wlValue <- numberOfFreqBins*2
-freqBinWidth <- 22050/numberOfFreqBins
+freqParameters(frequencyResolution)
 
 # Input: Nothing on the user's end. All values are generated from the previous functions.
 # Output: soundfileDuration gives the duration of the sound file in seconds. timeBinWidth gives the width of each time bin in seconds. numberOfTimeBins gives the number of time bins that will be created by the software.
-soundfileDuration <- (length(soundfileRead@left)/soundfileRead@samp.rate)
-timeBinWidth <- (2.98/64)/(1024/numberOfFreqBins) # ERROR: SOMETIMES NEEDS TO BE x2 THIS VALUE, UNKNOWN CAUSE AS OF June 11, 2017
-numberOfTimeBins <- round(soundfileDuration/timeBinWidth-0.5) # The -0.5 rounds the number down, as the later functions will do (it seems).
+timeParameters(soundfileActiveRead)
 
 # Input: The wlValue calculated previously and the active soundfile path (designated in STEP 4).
 # Output: A visual overview of the time envelope and frequency spectrum of the soundfileActive.
 acoustat(soundfileActiveRead, wl = wlValue)
 
-# STEP 6 - EXTRACT DATA
+# STEP 6 - EXTRACT FREQUENCYxTIME DATA
 # The following function will create the data table that shows the changing intensity values of frequencies over time.
 # Everything below the threshold value will be erased to reduce noise.
+soundAnalysis(file=soundfileActive, wl = wlValue, thresholdValue = thresholdValue)
 
-# Input: Previously calculated and designated values.
-# Output: Matrix with frequencies listed as rows and time listed as columns.
-soundMatrix <- stft.ext(file=soundfileActive, wl = wlValue) #, ovlp = 0.5)
-thresholdValue = 1
-thresholdFunction <- (function(x) if(x < thresholdValue) {x = 0} else x)
-soundMatrixThreshold <- soundMatrix # Creates a duplicate matrix
-soundMatrixThreshold[] <- vapply(soundMatrix, thresholdFunction, numeric(1)) # Applies the function to the duplicate
-
-# Input: The previous soundMatrixThreshold.
-# Output: A version with proper Hz and second values as row and column names, respectively.
-soundMatrixThresholdNamed <- soundMatrixThreshold
-
-# Give columns and rows a value that can later be converted into a name.
-# Creates sequences of the necessary length.
-col_seq = 1:ncol(soundMatrixThresholdNamed) 
-row_seq = 1:nrow(soundMatrixThresholdNamed)
-
-# Applies the values as names to the matrix
-colnames(soundMatrixThresholdNamed) <- c(paste(col_seq))
-rownames(soundMatrixThresholdNamed) <- c(paste(row_seq))
-
-# Name the X axis for frequency bins (the value is equal to the upper limit of each bin)
-colnames(soundMatrixThresholdNamed) <- c(paste(col_seq))
-for (i in col_seq) {colnames(soundMatrixThresholdNamed)[i] <- paste(
-  i*timeBinWidth
-)}
-
-# Name the Y axis for time bins (the value is equal to the upper limit of each bin)
-for (i in row_seq) {rownames(soundMatrixThresholdNamed)[i] <- paste(
-  i*freqBinWidth
-)}
 
 # STEP 7 - ISOLATE HARMONICS
+# Top-down method: User selects number of harmonics and the fundamental
 # Only the 16 (or specified) number of harmonics are required.
 # Input: The numberOfHarmonics and the fundamentalFreq values.
 # Outpit: A data table with only the rows of the desired harmonics.
 numberOfHarmonics <- 16 # This value can be changed as necessary.
 fundamentalFreq <- 261.63 # In Hz.
 
-harmonicSeq <- seq(from = fundamentalFreq, to = fundamentalFreq*numberOfHarmonics, by = fundamentalFreq)
-
-# Every row is the same frequency distance from the next = freqBinWidth
-startingBin <- as.integer(fundamentalFreq / freqBinWidth + 1) # +1 added to round up as it is needed for optimal results, based on trial and error
-
-harmonicColSeq <- seq(from = startingBin, to = startingBin*numberOfHarmonics, by = 12.2) # Based on horn and cello values
-
-soundMatrixThresholdNamedTransposed <- t(soundMatrixThresholdNamed)
-
-harmonicMatrix <- soundMatrixThresholdNamedTransposed[,harmonicColSeq[1:numberOfHarmonics]]
-
-maximumValue <- max(harmonicMatrix, na.rm = TRUE)
-scaleFunction <- (function(x) x/maximumValue)
-harmonicMatrixScaled <- harmonicMatrix # Creates a duplicate matrix
-harmonicMatrixScaled[] <- vapply(harmonicMatrix, scaleFunction, numeric(1)) # Scales all values to be between 0-1.
+isolateHarmonics() # Uses many arguments, so currently does not allow changes to arguments for simplicity of use
 
 # This creates all 16 harmonic arrays
-for(i in 1:numberOfHarmonics) {
-  temp_harmonicMatrixScaled_name = paste("harmonicMatrixScaled", i, "array", sep = ".")
+#for(i in 1:numberOfHarmonics) {
+#  temp_harmonicMatrixScaled_name = paste("harmonicMatrixScaled", i, "array", sep = ".")
 
-  temp_harmonicMatrixScaled_values <- paste(as.character(harmonicMatrixScaled[,i]), collapse=",")
+#  temp_harmonicMatrixScaled_values <- paste(as.character(harmonicMatrixScaled[,i]), collapse=",")
   
-  assign(temp_harmonicMatrixScaled_name, temp_harmonicMatrixScaled_values)
-}
+#  assign(temp_harmonicMatrixScaled_name, temp_harmonicMatrixScaled_values)
+#}
+
+
+# STEP 9 - EXPORT files
+# Input: All precalculated by previous functions.
+# Output: The exported TEXT files will be used by MAESTRO (SuperCollider component) to generate the synthesized sound.
 
 # The time array
-timeRawArray <- rep(timeBinWidth, each = (numberOfTimeBins-1))
-timeRawArray.print <- paste(as.character(timeRawArray), collapse=",")
-timeArray.print <- paste0("[", timeRawArray.print, "]")
+timeArray()
 
-# STEP 8 - EXPORT files
-# Input: All precalculated by previous functions.
-# Output: The exported CSV and TEXT files will be used by MAESTRO (SuperCollider component) to generate the synthesized sound.
-write.csv(harmonicMatrixScaled, file = "harmonicMatrixScaled.csv")
-
-
-##### filePathPrint <- paste0(filePath, "/R_working_files")
-
-for(i in 1:numberOfHarmonics) {
-  temp_harmonicMatrixScaled_print_name = paste("harmonicMatrixScaled", i, "array.print.txt", sep = ".")
-
-  temp_harmonicMatrixScaled_array_value <- paste("harmonicMatrixScaled", i, "array", sep = ".")
-
-  temp_harmonicMatrixScaled_values <- paste(as.character(harmonicMatrixScaled[,i]), collapse=",")
-  
-  temp_harmonicMatrixScaled_array_value <- temp_harmonicMatrixScaled_values
-  
-  temp_harmonicMatrixScaled_array_value_bounded <- paste0("[", temp_harmonicMatrixScaled_array_value, "]")
-  
-  write(temp_harmonicMatrixScaled_array_value_bounded, file = temp_harmonicMatrixScaled_print_name)
-}
-
-write(timeArray.print, file = "timeRawArray.print.txt")
+# Full print of two files, the harmonics table and the time array:
+exportMAESTRO()
 
 print("Script completed, switch to SuperCollider now.")
--- programs/R/functions_R.R
+++ programs/R/functions_R.R
@@ -1,6 +1,7 @@
 # This R code file contains all the functions used in the "envelope_generator.R" code file.
 # It acts as a source file, which is called at the beginning of "envelope_generator.R".
 # Inputs and outputs are commented before each function.
+# Note: <<- assigns as GLOBAL variable
 
 
 # STEP 1 - PACKAGES
@@ -19,34 +20,127 @@ loadPackages <- function(){
 
 # STEP 2 - SET WORKING DIRECTORY
 organizeDirectory <- function(x){
-  setwd(x) # Sets the working directory to the argument
-  filePathPrint <- paste0(x, "/R_working_files") # Filepath string for the folder to print the files
+  #setwd(x) # Sets the working directory to the argument
+  print(getwd())
+  filePathPrint <<- paste0(x, "/R_working_files") # Filepath string for the folder to print the files
 }
 
 # STEP 4 - SOUND CHECKING
-soundfileStereoCheck <- function(x){
+soundfileStereoCheck <- function(x) {
   soundfileRead <- readWave(x)
   if(soundfileRead@stereo) {
     soundfileMonoL <- mono(soundfileRead, "left") # To save the right channel instead, change "left" to "right"
     setwd(filePathPrint) # Temporarily setting the place to save the files
     savewav(soundfileMonoL)
     print(paste0("The selected sound file was a stereo sound. Currently only mono sounds are usable by this script. A WAVE file 'soundfileMonoL.wav' has been created in the directory '", getwd(), "'. The following computations will use this newly created mono-left sound version of the original sound file."))
-    soundfileActive <- (paste0(getwd(), "/", "soundfileMonoL.wav"))
+    soundfileActive <<- (paste0(getwd(), "/", "soundfileMonoL.wav"))
   } else {
-    soundfileActive <- x
+    soundfileActive <<- x
     print("The selected sound file was a mono sound. No changes necessary.")
   }
-  soundfileActiveRead <- readWave(soundfileActive) 
+  soundfileActiveRead <<- readWave(soundfileActive) 
 }
 
 
+# STEP 5 - PARAMETER SETTING
+freqParameters <- function(x) { # Input argument: 
+  numberOfFreqBins <<- 256*x
+  wlValue <<- numberOfFreqBins*2
+  freqBinWidth <<- 22050/numberOfFreqBins
+}
+
+timeParameters <- function(x) { # Input argument: soundfileActiveRead
+  soundfileDuration <<- (length(x@left)/x@samp.rate) # The sound should be mono so @left and @right should not matter
+  timeBinWidth <<- (2.98/64)/(1024/numberOfFreqBins) # ERROR: SOMETIMES NEEDS TO BE x2 THIS VALUE, UNKNOWN CAUSE AS OF June 11, 2017
+  numberOfTimeBins <<- round(soundfileDuration/timeBinWidth-0.5) # The -0.5 rounds the number down, as the later functions will do (it seems).
+}
+
+soundAnalysis <- function(file, wl, thresholdValue) { # Input arguments: file=soundfileActive, wl = wlValue, thresholdValue = thresholdValue)
+  # Input: Previously calculated and designated values.
+  # Output: Matrix with frequencies listed as rows and time listed as columns.
+  soundMatrix <- stft.ext(file=soundfileActive, wl = wlValue) #, ovlp = 0.5)
+  #thresholdValue = 1
+  thresholdFunction <- (function(x) if(x < thresholdValue) {x = 0} else x)
+  soundMatrixThreshold <- soundMatrix # Creates a duplicate matrix
+  soundMatrixThreshold[] <- vapply(soundMatrix, thresholdFunction, numeric(1)) # Applies the function to the duplicate
+  
+  # Input: The previous soundMatrixThreshold.
+  # Output: A version with proper Hz and second values as row and column names, respectively.
+  soundMatrixThresholdNamed <- soundMatrixThreshold
+  
+  # Give columns and rows a value that can later be converted into a name.
+  # Creates sequences of the necessary length.
+  col_seq = 1:ncol(soundMatrixThresholdNamed) 
+  row_seq = 1:nrow(soundMatrixThresholdNamed)
+  
+  # Applies the values as names to the matrix
+  colnames(soundMatrixThresholdNamed) <- c(paste(col_seq))
+  rownames(soundMatrixThresholdNamed) <- c(paste(row_seq))
+  
+  # Name the X axis for frequency bins (the value is equal to the upper limit of each bin)
+  colnames(soundMatrixThresholdNamed) <- c(paste(col_seq))
+  for (i in col_seq) {colnames(soundMatrixThresholdNamed)[i] <- paste(
+    i*timeBinWidth
+  )}
+  
+  # Name the Y axis for time bins (the value is equal to the upper limit of each bin)
+  for (i in row_seq) {rownames(soundMatrixThresholdNamed)[i] <- paste(
+    i*freqBinWidth
+  )}
+  
+  soundMatrixThresholdNamed <<- soundMatrixThresholdNamed # Saves to Global Environment
+}
+
+# STEP 7 - ISOLATE HARMONICS
+isolateHarmonics <- function() {
+  harmonicSeq <- seq(from = fundamentalFreq, to = fundamentalFreq*numberOfHarmonics, by = fundamentalFreq)
+  
+  # Every row is the same frequency distance from the next = freqBinWidth
+  startingBin <- as.integer(fundamentalFreq / freqBinWidth + 1) # +1 added to round up as it is needed for optimal results, based on trial and error
+  
+  harmonicColSeq <- seq(from = startingBin, to = startingBin*numberOfHarmonics, by = 12.2) # Based on horn and cello values
+  
+  soundMatrixThresholdNamedTransposed <- t(soundMatrixThresholdNamed)
+  
+  harmonicMatrix <- soundMatrixThresholdNamedTransposed[,harmonicColSeq[1:numberOfHarmonics]]
+  
+  maximumValue <- max(harmonicMatrix, na.rm = TRUE)
+  scaleFunction <- (function(x) x/maximumValue)
+  harmonicMatrixScaled <- harmonicMatrix # Creates a duplicate matrix
+  harmonicMatrixScaled[] <- vapply(harmonicMatrix, scaleFunction, numeric(1)) # Scales all values to be between 0-1.
+  
+  harmonicMatrixScaled <<- harmonicMatrixScaled
+}
+
+# STEP 8 - PLOT
+# Uses PLOTLY
 
 
-#mult <- function(x,y) {
-#  x*y
-#}
 
 
+# STEP 9 - EXPORT files
+timeArray <- function() {
+  
+  timeRawArray <- rep(timeBinWidth, each = (numberOfTimeBins-1))
+  timeRawArrayPrint <<- paste(as.character(timeRawArray), collapse=",")
+  timeArrayPrint <<- paste0("[", timeRawArrayPrint, "]")
+  
+}
+
+exportMAESTRO <- function() { #CSV = FALSE, TAB = TRUE) { NOTE: In the future perhaps allow multiple versions of export
+  #if(CSV = TRUE)
+    
+  #write.csv(harmonicMatrixScaled, file = "harmonicMatrixScaled.csv")
+  #setwd(filePathPrint)
+  dataFileName_harmonicMatrixScaled <- paste0(filePathPrint, "/harmonicMatrixScaled.txt")
+  #dataFileName_timeRawArray <- paste0(filePathPrint, "/timeRawArray.txt")
+  dataFileName_timeArray <- paste0(filePathPrint, "/timeArray.txt")
+  
+  write.table(t(harmonicMatrixScaled), file = dataFileName_harmonicMatrixScaled, sep="\t")
+  #write.table(timeRawArray, file = dataFileName_timeRawArray)
+  
+  write(timeArrayPrint, file = dataFileName_timeArray) # NOTE: Ideally won't need in future if SC can read the time (row) names, or just generate based of some values of time interval
+}
 
 # If this source file of functions ("functions_R.R") has finished loading into the "envelope_generator.R", print the follow line
 print("Source functions loaded from 'functions_R.R'.")
